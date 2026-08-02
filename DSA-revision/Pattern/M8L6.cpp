4c10550ce9e7fb2e4d8f98224d199fd8c6082936
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-02 18:02:30 +0000

    mirror image of star right angled triangle

--- DSA-revision/Pattern/M8L6.cpp
+++ DSA-revision/Pattern/M8L6.cpp
@@ -47,4 +47,14 @@ int main()
         }
         cout << endl;
     }
+    cout<<endl<<"mirror image of star trianglr"<<endl;
+    for(int i =0;i<n;i++)
+    {
+        for(int j= 0 ;j<n;j++)
+        {
+            if(i+j>=n-1) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
 }
