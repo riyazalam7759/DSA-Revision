b5ea7b43450c8ebb6610382546cc4d74d1150d61
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-02 17:04:55 +0000

    0 and 1 triangle with alternate values

--- DSA-revision/Pattern/M8L6.cpp
+++ DSA-revision/Pattern/M8L6.cpp
@@ -16,6 +16,20 @@ int main()
         }
         cout<<endl;
     }
+    int m=1;
+    cout<<endl<<"0 and 1 triangle"<<endl;
+    for(int i=0;i<n;i++)
+    {
+         if(i%2==0) m=1;
+         else  m=0;
+        for(int j=0;j<=i;j++)
+        {
+            cout<<m<<" ";
+            if(m==1) m=0;
+            else m=1;
+        }
+        cout<<endl;
+    }
 
 }
 
