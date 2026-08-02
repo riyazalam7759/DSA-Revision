ffed1a3f77367d6eeae705303d2c89d78497549e
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-02 12:54:23 +0000

    number triangle

---
+++ DSA-revision/Pattern/M8L6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ,a=1 ;
+    cout<<"enter a number :";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<a<<" ";
+            a+=1;
+
+        }
+        cout<<endl;
+    }
+
+}
+
+
