ccaf61bebaffe8bdcc6369339b6e0a7aff871983
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-02 12:37:59 +0000

    added cross star pattern also in this file

--- DSA-revision/Pattern/M8L5starPlus.cpp
+++ DSA-revision/Pattern/M8L5starPlus.cpp
@@ -6,6 +6,7 @@ int main()
     int n;
     cout<<"enter any odd number :";
     cin>>n;
+    cout<<"star plus pattern"<<endl;
     for (int i = 0; i< n;i++)
     {
         for(int j=0;j<n;j++)
@@ -18,6 +19,16 @@ int main()
             {
                 cout<<" ";
             }
+        }
+        cout<<endl;
+    }
+    cout<<endl<<"cross star pattern"<<endl;
+    for (int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==j || i+j==n-1) cout<<"*";
+            else cout<<" ";
         }
         cout<<endl;
     }
