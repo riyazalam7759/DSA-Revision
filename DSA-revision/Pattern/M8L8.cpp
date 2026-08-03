0d2861182a11dbe14c072040280f0710f998857a
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-03 12:05:59 +0000

    method 2 has a small bug

--- DSA-revision/Pattern/M8L8.cpp
+++ DSA-revision/Pattern/M8L8.cpp
@@ -15,5 +15,12 @@ int main()
         }
         cout<<endl;
     }
+    cout<<endl<<"method 2 of pyramid"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++) cout<<" ";
+        for(int k=0;k<2*i-1;k++) cout<<"*";
+        cout<<endl;
+    }
 }
 
