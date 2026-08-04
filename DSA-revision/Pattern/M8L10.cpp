184d098ec8b068087d3ef91fcbf49ae777db6814
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-04 06:35:08 +0000

    printed number bridge

--- DSA-revision/Pattern/M8L10.cpp
+++ DSA-revision/Pattern/M8L10.cpp
@@ -33,6 +33,38 @@ int main()
         cout<<endl;
     }
 
+    m=n-1;
+     nsp=1;
+     for(int i=1;i<2*n;i++)
+     {
+        cout<<i;
+     }
+     cout<<endl;
+    for(int i=1;i<=m;i++)
+    {
+        int a=1;
+        for(int j=1;j<=m+1-i;j++)
+        {
+            cout<<a;
+            a++;
+        }
+        for(int k=1;k<=nsp;k++)
+        {
+            cout<<" ";
+            a++;
+        }
+        nsp+=2;
+        for(int j=1;j<=m+1-i;j++)
+        {
+            cout<<a;
+            a++;
+        }
+        cout<<endl;
+    
+    }
+
+    
+
 }
 
 
