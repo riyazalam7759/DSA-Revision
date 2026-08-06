31f97ec404063fda7c1d9b6a514be766d2dfa449
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-06 12:24:22 +0000

    taking input with the help of pointers

--- DSA-revision/pointer/M10L3.cpp
+++ DSA-revision/pointer/M10L3.cpp
@@ -10,5 +10,15 @@ int main()
     int * p3=&c;
     *p3=*p1+*p2;
     cout<<"The sum is : "<<*p3<<endl;
+    int d,e;
+    int *p4=&d;
+    int *p5=&e;
+    cout<<"enter 1st num :";
+    cin>>*p4;
+    cout<<"enter 2nd num :";
+    cin>>*p5;
+    cout<<"the sum is :"<<*p4+*p5<<endl;
+      cout<<"the sum is :"<<d+e<<endl;
 
 }
+
