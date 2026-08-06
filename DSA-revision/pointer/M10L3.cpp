e805911dfb7d493d44572cacd10600a5aac6c77e
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-06 12:37:10 +0000

    taking input using pointer and seeing some syntax

--- DSA-revision/pointer/M10L3.cpp
+++ DSA-revision/pointer/M10L3.cpp
@@ -19,6 +19,15 @@ int main()
     cin>>*p5;
     cout<<"the sum is :"<<*p4+*p5<<endl;
       cout<<"the sum is :"<<d+e<<endl;
-
+      /*
+      int *p1, p2 ; =>here p1 is an int pointer but p2 is an int variable
+      */
+     int *q1=&d, *q2=&e ; //=>here both p1 and p2 are int pointers
+     cout<<q1<<" "<<q2<<endl;
+     int *q3, *q4 ; //=>here q3 is an int pointer but q4 is an int variable
+     q3=&d;
+     q4=&e;
+     cout<<q3<<" "<<q4<<endl;
+     //all are correct syntax
 }
 
