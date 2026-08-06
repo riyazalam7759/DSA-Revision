0ed1096c720232d013c956b47fe449f8efe20f06
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-06 11:58:52 +0000

    just playing with pointers

---
+++ DSA-revision/pointer/M10L2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=4;
+    int *p=&x;
+    cout<<&x << " " << p<<endl;//=>both give same address
+    bool y=true;
+    bool *q=&y;
+
+
+}
+
