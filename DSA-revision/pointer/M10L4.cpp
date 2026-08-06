d340d3ba01f1c8ba3df2b4f460fded29c5b0675c
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-06 17:40:01 +0000

    swapping two values using pass by reference and pointer

--- DSA-revision/pointer/M10L4.cpp
+++ DSA-revision/pointer/M10L4.cpp
@@ -2,6 +2,13 @@
 #include<iostream>
 using namespace std;
 
+void swap2(int &a , int &b)//=>pass by reference using reference variable
+{
+    int  temp= a;
+    a=b;
+    b=temp;
+    return;
+}
 void swap(int *a , int *b)
 {
     int  temp= *a;
@@ -15,7 +22,10 @@ int main()
 {
     int a=5 , b=7;
     swap(&a , &b);
-    cout<<a<<" "<<b;
+    cout<<a<<" "<<b<<endl;
+    int x=3 , y=12;
+    swap2(x , y);
+    cout<<x<<" "<<y<<endl;
 
 }
 
