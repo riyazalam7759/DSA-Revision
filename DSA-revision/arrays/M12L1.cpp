009f3778148ee32e049b5e70d11b98e78e292f6a
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-08 11:41:40 +0000

    added some notes and passed the size of array to function

--- DSA-revision/arrays/M12L1.cpp
+++ DSA-revision/arrays/M12L1.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 //both array a in display and array b in change functions are point to one array which is arr
 //so if we make change in anyone then it will the array 
-void display(int a[])//this passing of array is pass by reference
-{
-    for(int i =0;i<5;i++)
+void display(int a[],int size)//this passing of array is pass by reference
+{//here we can say that 'a' is a pointer which point arr
+    //so here "int size=sizeof(a)/sizeof(a[0])" => may give error 
+    for(int i =0;i<size;i++)
     {
         cout<<a[i]<<" ";
     }
@@ -18,10 +19,10 @@ void change(int b[])
 int main()
 {
     int arr[5] = {1,2,3,4,5};
-    display(arr);
+    display(arr,5);
     change(arr);
     cout<<endl;
-    display(arr);
+    display(arr,5);
 }
 
 
