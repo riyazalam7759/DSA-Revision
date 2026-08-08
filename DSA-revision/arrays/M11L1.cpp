871f9d9c5624830c460390775eebb1ec7bde6cfa
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-08 06:49:42 +0000

    basic of array

--- DSA-revision/arrays/M11L1.cpp
+++ DSA-revision/arrays/M11L1.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
+    int arr[]={10,11,12,13,14,15,16,17,18,19};
     int x[5];
     int y[4]={1,2,3,4};
     x[3]=10;
     x[0]=5;
+    int n=sizeof(arr)/sizeof(arr[0]);
 }
 
 
