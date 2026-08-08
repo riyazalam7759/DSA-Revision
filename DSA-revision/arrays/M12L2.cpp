6860c2210b8eb189ee37d3e807db02ba8b0ab372
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-08 12:22:22 +0000

    some important note about pointer

--- DSA-revision/arrays/M12L2.cpp
+++ DSA-revision/arrays/M12L2.cpp
@@ -12,11 +12,39 @@ int main()
     //int *ptr =arr2[0]; =>this will also give error 
     cout<<arr;
     cout<<endl<<ptr1[2]<<endl;//=>3
-    ptr[1]='m';//changed 'b' into 'm'
+    ptr[1]='m';//changed 'b' into 'm' 
     for(int i=0;i<6;i++)
     {
         cout<<ptr[i]<<" ";//it will display all the character
     }
+
+    cout<<endl;
+    for(int i=0;i<6;i++)
+    {
+        cout<<*ptr1<<" ";//print all the elements
+        ptr1++;
+    }
+    ptr1=arr2;
+    *ptr1=9;//ptr[0]=9;
+    ptr1++;
+    *ptr1=10;
+    ptr1--;
+cout<<endl;
+     for(int i=0;i<6;i++)
+    {
+        cout<<*ptr1<<" ";//print all the elements after changing value
+        ptr1++;
+    }
+cout<<endl;
+     for(int i=0;i<6;i++)
+    {
+        cout<<i[arr]<<" ";//we can also print this way
+        
+    }
+
+
+
+
 }
 
 
