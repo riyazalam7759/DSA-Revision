f039dd28999b0b41925b4a3a2dbfa3bdf0f05c4d
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-08 12:23:27 +0000

    some patterns

--- DSA-revision/Pattern/pattern.cpp
+++ DSA-revision/Pattern/pattern.cpp
@@ -10,6 +10,33 @@ int main(){
         cout<<endl;
     
     }
+    for(int i=0;i<4;i++)//triangle pattern
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl<<"inverted triangle"<<endl;
+    for(int i=0;i< 4;i++)//inverted triangle pattern
+    {
+        for(int j=i;j<4;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl<<"hollow rectangle"<<endl;
+    for(int i = 0 ; i< 5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            if(i==0 || j==0 || i==4 || j==4) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
 
 
 }
