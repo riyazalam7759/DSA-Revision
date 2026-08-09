16ad115f20c3dbab4971bc9c2720661cd8c68d83
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-09 07:13:02 +0000

    taking input in the vector

--- DSA-revision/vector/M12L4.cpp
+++ DSA-revision/vector/M12L4.cpp
@@ -25,7 +25,7 @@ int main()
     for(int i=0;i<v.size();i++)
     cout<<v[i]<<" ";
 
-    cout<<endl<<"vector with size :";
+    cout<<endl<<endl<<"vector with size :";
     vector<int> v2(5);//=>the size of this vector is 5 and capacity is also 5 and get default value 0
     cout<<endl<<"size :"<<v2.size()<<" capacity :"<<v2.size();//=>both will be 5 
 
@@ -34,6 +34,26 @@ int main()
     vector<int> v3(7,100);//=>declared size and capacity is 7 but default value of each element is 100
     cout<<endl<<v3[4]<<" "<<v3[6];
 
+    cout<<endl<<endl<<"taking input in vector";
+    vector<int> v4(4);
+    cout<<endl<<"enter the four elements in vector :";
+    for(int i =0;i<4;i++)
+    {
+        cin>>v4[i];//=>we cant take input this way if we havent give the size of vector during declaration
+    }
+    cout<<"elements are :";
+    for(int i =0;i<4;i++) cout<<v4[i]<<" ";
+    cout<<endl<<"taking input without declaring size enter six element :";
+    vector<int> v5;
+    for(int i=0;i<6;i++)
+    {
+        int ele;
+        cin>>ele;
+        v5.push_back(ele);
+    }
+    cout<<"elements are :";
+    for(int i=0;i<6;i++) cout<<v5[i]<<" ";
+    
 }
 
 
