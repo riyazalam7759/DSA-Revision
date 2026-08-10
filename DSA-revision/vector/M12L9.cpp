1e06016cf8c692ed43427d65a612f9ce60c0abbc
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-10 17:33:01 +0000

    added second method to sort 0 and 1 array using number of zeros and number of one

--- DSA-revision/vector/M12L9.cpp
+++ DSA-revision/vector/M12L9.cpp
@@ -19,6 +19,7 @@ int main()
     v.push_back(1);
     v.push_back(0);
     v.push_back(1);
+    v.push_back(0);
     int i=0;
     int j=v.size()-1;
     while(i<=j)
@@ -35,6 +36,30 @@ int main()
         if(v[j]==1) j--;
     }
     display(v);
+
+    cout<<endl<<"second method"<<endl;
+     vector<int> v2;
+    v2.push_back(1);
+    v2.push_back(0);
+    v2.push_back(0);
+    v2.push_back(1);
+    v2.push_back(0);
+    v2.push_back(1);
+    v2.push_back(0);
+    int noz =0;//number of zeros
+    int noo =0;//number of one
+    for(int i=0;i<v2.size();i++)
+    {
+        if(v2[i]==0) noz++;
+        if(v2[i]==1) noo++;
+    }
+    for(int i=0;i<noz;i++)
+        v2[i]=0;
+    for(int i=noz;i<noz+noo;i++)
+        v2[i]=1;
+    display(v2);
+    
+
 }
 
 
