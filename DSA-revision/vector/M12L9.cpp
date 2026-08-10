47079e467ea4d1902035d375704d492034edb46d
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-10 18:14:50 +0000

    just changers input

--- DSA-revision/vector/M12L9.cpp
+++ DSA-revision/vector/M12L9.cpp
@@ -14,10 +14,11 @@ int main()
 {
     vector<int> v;
     v.push_back(1);
-    v.push_back(0);
+    v.push_back(1);
     v.push_back(0);
     v.push_back(1);
     v.push_back(0);
+    v.push_back(1);
     v.push_back(1);
     v.push_back(0);
     int i=0;
