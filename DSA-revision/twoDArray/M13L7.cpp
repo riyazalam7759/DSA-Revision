fe6216395de7c97189b05c106f16f200483023f2
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-14 13:09:51 +0000

    storing transpose matrix into a new matrix

--- DSA-revision/twoDArray/M13L7.cpp
+++ DSA-revision/twoDArray/M13L7.cpp
@@ -2,25 +2,55 @@
 using namespace std;
 int main()
 {
-    int arr[2][3]={1,2,3,4,5,6};
-    cout<<endl<<"normal matrix :"<<endl;
-    for(int i=0;i<2;i++)
+    int n , m;
+    cout<<"enter rows and colums :";
+    cin>>n>>m;
+    int arr[n][m];
+    cout<<endl<<"enter the element in the matrix :";
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl<<"normal matrix :"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
         {
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
     cout<<"transpose matrix :"<<endl;
-    for(int j=0;j<3;j++)
+    for(int j=0;j<m;j++)
     {
-        for(int i=0;i<2;i++)
+        for(int i=0;i<n;i++)
         {
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
+    //storing transpose matrix in new matrix
+    int t[m][n];
+    for(int i =0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            t[i][j]=arr[j][i];
+        }
+    }
+    cout<<endl<<"printing transpose matrix stored in new matrix :"<<endl;
+     for(int i =0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+           cout<< t[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
 
     
 }
