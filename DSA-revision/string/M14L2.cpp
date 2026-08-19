69b25032c2e018d1d2143c2d9bee17c8583dfce1
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-19 06:08:08 +0000

    upadting string elements  . in c++ string is mutable

---
+++ DSA-revision/string/M14L2.cpp
@@ -0,0 +1,12 @@
+//updation of a single string character in string 
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str="riyaz";
+    str[2]='f';
+    cout<<str;//=>rifaz
+    //so in c++ strings are mutable 
+}
+
