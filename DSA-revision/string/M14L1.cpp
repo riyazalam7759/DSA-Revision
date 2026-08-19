687015786ec8fe5fb00847bf8afd96de2a892b4c
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-19 05:49:52 +0000

    more notes added for string

--- DSA-revision/string/M14L1.cpp
+++ DSA-revision/string/M14L1.cpp
@@ -1,5 +1,6 @@
 //String 
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
@@ -15,7 +16,23 @@ int main()
     cout<<endl<<(int)str2[5];//=> 0 cause at the end there is '\0' character
     cout<<endl<<str;// this will print => "abcd"
 
-    
+    cout<<endl<<endl<<"another declaration of string ";
+    string str4="abcde";
+    cout<<endl<<str4;//=>abcde
+    cout<<endl<<str4[0];//=>a
+
+    cout<<endl<<endl<<"taking user input in string ";
+    string s;
+    cin>>s;//if you give space the till that space will be printed means only one word will be printed
+    cout<<endl<<"user input string is :"<<s;
+    //but 
+    string s2="this is a good boy";
+    cout<<endl<<s2;//=> this is a good boy 
+     string s3;
+     getline(cin,s3);//=> this will also take spaces as input
+     cout<<s3;//=>including space
+
+
 }
 
 
