c548aba1538c6234a1a7bc4da9b2aa18bdb8aece
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-08-26 12:08:25 +0000

    added one more functtion to remove a character

--- DSA-revision/recursion/M17L1p2.cpp
+++ DSA-revision/recursion/M17L1p2.cpp
@@ -8,11 +8,24 @@ string rmvChar(string s,string t,int i)
     i++;
     return rmvChar(s,t,i);
 }
+//second function
+void removeChar(string ans , string original)
+{
+    if(original.length()==0) 
+    {
+        cout<<ans;
+        return;
+    }
+    char ch=original[0];
+    if(ch=='a') removeChar(ans,original.substr(1));
+    else removeChar(ans+ch,original.substr(1));
+}
 int main()
 {
     string s="membermost";
     cout<<"before removing m :"<<s;
-    cout<<endl<<"after removing m :"<<rmvChar(s,"",0);
+    cout<<endl<<"after removing m :"<<rmvChar(s,"",0)<<endl;
+    removeChar("",s);
 }
 
 
