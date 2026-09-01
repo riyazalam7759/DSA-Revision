fdb931e1bb2d32fa082de658d0f150cb09d39622
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-01 07:18:36 +0000

    printing permutations of a string

---
+++ DSA-revision/recursion/M17L7p2.cpp
@@ -0,0 +1,25 @@
+//finding all permutation of an string given all elements of the string are unique 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void subStringPermutations(string s,string ans)
+{
+    if(s.size()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+    for(int j=0;j<s.size();j++)
+    {
+        string t=s.substr(0,j)+s.substr(j+1,s.size());
+        subStringPermutations(t,ans+s[j]);
+    } 
+}
+int main()
+{
+    string s="abcd";
+    string ans="";
+    subStringPermutations(s,ans);
+}
