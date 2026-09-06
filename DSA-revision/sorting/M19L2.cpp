0715c7515b0e881f063948599283ec0f41488914
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-06 06:52:46 +0000

    most optimal bubble sort , what if array get already sorted then what is the point to run the passes so break the loop

--- DSA-revision/sorting/M19L2.cpp
+++ DSA-revision/sorting/M19L2.cpp
@@ -3,21 +3,29 @@
 using namespace std;
 int main()
 {
-    int arr[]={5,4,6,3,2,1};
+    int count=1;
+    int arr[]={3,4,2,1,5,7,0};
+    bool flag=true;
     int n=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n-1;i++)
     {
         for(int j=0;j<n-1-i;j++)
-        {
+        {  
             if(arr[j]>arr[j+1])
-            swap(arr[j],arr[j+1]);
+            {
+                flag=false;
+                swap(arr[j],arr[j+1]);
+            }
         }
+        if(flag==true) break;
+        count++;
 
     }
     for(int i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
     }
+    cout<<endl<<"total number of pass or number of times outer loop runs :"<<count;
 
 
 }
