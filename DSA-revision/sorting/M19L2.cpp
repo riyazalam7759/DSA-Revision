af16685b0e7053da5389c4ee205305909d567cd9
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-06 12:09:57 +0000

    just small fixing

--- DSA-revision/sorting/M19L2.cpp
+++ DSA-revision/sorting/M19L2.cpp
@@ -8,7 +8,7 @@ int main()
     bool flag=true;
     int n=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n-1;i++)
-    {
+    {    
         for(int j=0;j<n-1-i;j++)
         {  
             if(arr[j]>arr[j+1])
@@ -19,13 +19,11 @@ int main()
         }
         if(flag==true) break;
         count++;
-
+        flag =true;
     }
     for(int i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
     }
-    cout<<endl<<"total number of pass or number of times outer loop runs :"<<count;
-
-
+    cout<<endl<<"total number of pass or number of times outer loop runs :"<<count<<flag;
 }
