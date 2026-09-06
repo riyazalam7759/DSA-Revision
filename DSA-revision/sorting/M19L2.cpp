d0b8cf0c29fce37b50147e0c7ec75699e81c6deb
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-06 06:16:37 +0000

    this is optimal solution for the bubble for the bubble sort

--- DSA-revision/sorting/M19L2.cpp
+++ DSA-revision/sorting/M19L2.cpp
@@ -7,7 +7,7 @@ int main()
     int n=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n-1;i++)
     {
-        for(int j=0;j<n-1;j++)
+        for(int j=0;j<n-1-i;j++)
         {
             if(arr[j]>arr[j+1])
             swap(arr[j],arr[j+1]);
