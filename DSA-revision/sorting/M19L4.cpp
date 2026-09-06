46b5a939ac54d94012c32e071066a3e6bcca6e7b
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-06 07:43:12 +0000

    push zeroes to end while maintaining the relative order of other elements

---
+++ DSA-revision/sorting/M19L4.cpp
@@ -0,0 +1,29 @@
+//push zeroes to end while maintaining the relative order of other elements
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the size of array :";
+    cin>>n;
+    int arr[n];
+    cout<<endl<<"enter the element of the array :";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]==0)
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }  
+}
