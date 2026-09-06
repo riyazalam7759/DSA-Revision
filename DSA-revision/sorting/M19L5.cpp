f71f7969cb24ab4f4d981c1c374bf26fcb9ba64f
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-06 11:12:27 +0000

    selection sorting , first select smallest element then swap with its deserving index element

---
+++ DSA-revision/sorting/M19L5.cpp
@@ -0,0 +1,28 @@
+
+#include<climits>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[]={3,6,2,9,0,1,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n-1;i++)
+    {
+        int minNo=INT_MAX;
+         int idx=-1;
+        for(int j=i;j<n;j++)
+        {
+           if(minNo>arr[j])
+           {
+            minNo=arr[j];
+            idx=j;
+           }
+        }
+        swap(arr[i],arr[idx]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
