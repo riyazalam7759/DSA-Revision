dee46716dded7325e746b30abd1803438564cd8b
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-12 13:23:41 +0000

    converting decimal to binary using

---
+++ DSA-revision/bitwiseOperators/M23L3.cpp
@@ -0,0 +1,24 @@
+//converting decimal to binary using 
+#include <iostream>
+using namespace std;
+string decimalToBinary(int num)
+{
+    string result="";
+   while(num>0)
+   {
+    if(num%2==0)
+      result='0'+result;
+    else 
+      result='1'+result;
+    num /=2;
+   }
+    return result;
+}
+int main()
+{
+    int n;
+    cout<<"enter a num :";
+    cin>>n;
+    cout<<decimalToBinary(n);
+
+}
