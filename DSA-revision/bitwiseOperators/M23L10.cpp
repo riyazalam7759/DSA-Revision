69386a16f44ea09823d23c51b67b8c7abd2220d2
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-13 12:29:17 +0000

    all the elements in the array present twice but two numbers present in once only

---
+++ DSA-revision/bitwiseOperators/M23L10.cpp
@@ -0,0 +1,37 @@
+//finding two numbers which are present only once in the array 
+#include <iostream>
+using namespace std;
+void findUnique(int arr[], int n)
+{
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        res = res ^ arr[i];
+    }
+    int temp = res;
+    int k = 0;
+    while (true)
+    {
+        if ((temp & 1) == 1)
+        {
+            break;
+        }
+        temp=temp>>1;
+        k++;
+    }
+    int retval=0;
+    for(int i=0;i<n;i++)
+    {
+        int num=arr[i];
+        if(((num>>k)&1)==1) retval ^=num;
+    }
+    cout<<retval<<" ";
+    res=retval ^ res;
+    cout<<res<<" ";
+}
+int main()
+{
+    int arr[]={1,2,1,3,2,5,9,8,8,3};
+    int n=10;
+    findUnique(arr,n); 
+}
