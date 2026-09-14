d71c9eb130054a9c1c43c28f5a093da3e3d13532
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-14 05:33:14 +0000

    just modified

--- DSA-revision/Math/M24L1.cpp
+++ DSA-revision/Math/M24L1.cpp
@@ -2,17 +2,27 @@
 #include<iostream>
 #include<cmath>//for sqrt() function
 using namespace std;
-
+int fac=0;
 bool isPrime(int n)
 {
     if(n==1) return false ;
-    for(int i=2;i<n;i++) //int place of i<n we can write like i<sqrt(n)
+    for(int i=2;i<n;i++) //int place of i<n we can write like i<=sqrt(n)
     {
-        if(n%i==0) return false;
+        if(n%i==0) 
+        {
+            fac=i;
+            return false;
+        }
     }
     return true;
 }
 int main()
 {
-    cout<<isPrime(13)<<endl;
+    int n;
+    cout<<" enter number :";
+    cin>>n;
+    if(isPrime(n)==0)
+    cout<<fac<<" is first factorial of "<<n<<" and not a prime number "<<endl;
+    else 
+    cout<<n<<" is not a prime number ";
 }
