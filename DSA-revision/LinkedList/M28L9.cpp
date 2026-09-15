c826b577f23095c7e19573052e3b7445f0b39ed9
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-15 17:40:47 +0000

    implemented function to insert at head and insert at any index

---
+++ DSA-revision/LinkedList/M28L9.cpp
@@ -0,0 +1,97 @@
+
+#include <iostream>
+using namespace std;
+class Node
+{
+    public:
+      int val;
+      Node* next;
+      Node(int val)
+      {
+        this->val=val;
+        this->next=NULL;
+      }
+
+};
+class LinkedList
+{
+    public:
+
+    Node* head;
+    Node* tail;
+    int size;
+    LinkedList()
+    {
+        head=tail=NULL;
+        size=0;
+    }
+    void insertAtEnd(int val)
+    {
+        Node* temp=new Node(val);
+        if(size==0) head=tail=temp;
+        else
+        {
+            tail->next=temp;
+            tail=temp;
+        }
+        size++;
+
+    }
+    void display()
+    {
+        Node* temp=head;
+        while(temp != NULL)
+        {
+            cout<<temp->val<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    void insertAtHead(int val)
+    {
+        Node* temp=new Node(val);
+        if(size==0) head=tail=temp;
+        else
+        {
+            temp->next=head;
+            head=temp;
+        }
+        size++;
+    }
+    void insertAtIdx(int idx,int val)
+    {
+        if(idx<0 || idx> size) 
+        cout<<" invalid index"<<endl;
+        else if(idx==0) insertAtHead(val);
+        else if(idx==size) insertAtEnd(val);
+        else 
+        {
+           
+            Node *temp=new Node(val);
+            Node *t=head;
+            for(int i=0;i<idx-1;i++)
+            {
+                t=t->next;
+            }
+             
+             temp->next=t->next;
+             t->next=temp;
+             size++;
+        } 
+    }
+};
+int main()
+{
+    LinkedList ll;
+    ll.insertAtEnd(10);
+    ll.insertAtEnd(20);
+    ll.display();
+    cout<<endl<<"size is : "<<ll.size<<endl;
+    ll.insertAtHead(30);
+    ll.display();
+    ll.insertAtIdx(2,50);
+    ll.display();
+
+
+    
+}
