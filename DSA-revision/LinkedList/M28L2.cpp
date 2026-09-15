2fda1a356153c7cf9f76c0c60ce0776d6e6874e0
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-15 11:13:26 +0000

    forming linked list

---
+++ DSA-revision/LinkedList/M28L2.cpp
@@ -0,0 +1,27 @@
+//forming link list 
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* next;
+};
+
+int main()
+{
+    //10 , 20 , 30 , 40
+    Node a;
+    a.val=10;
+    Node b;
+    b.val=20;
+    Node c;
+    c.val=30;
+    Node d;
+    d.val=40;
+    //forming linked list 
+    a.next=&b;
+    b.next=&c;
+    c.next=&d;
+    d.next=NULL;
+}
+
