5e5194f9d70ccf95ad95b2ed8b94347c47643e93
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-15 12:44:15 +0000

    display and size funtionn for linked list

---
+++ DSA-revision/LinkedList/M28L5.cpp
@@ -0,0 +1,49 @@
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node(int val)
+        {
+            this->val=val;
+            this->next=NULL;
+        }
+};
+void display(Node* head)
+{
+    Node* temp=head;
+    while(temp != NULL)
+    {
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void size(Node * head)
+{
+    int n=0;
+    Node *temp=head;
+    while(temp != NULL)
+    {
+        n++;
+        temp=temp->next;
+    }
+    cout<<" size of the linked list is : "<<n;
+    cout<< endl;
+}
+int main()
+{
+    Node* a=new Node(10);
+    Node* b=new Node(20);
+    Node* c=new Node(30);
+    Node* d=new Node(40);
+
+    a->next=b;
+    b->next=c;
+    c->next=d;
+   
+    display(a);
+    size(a);
