bf11a782e813a06ab39f79d2ca93e2a9ae8bcc84
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-15 11:53:32 +0000

    printing value of the linked list using while loop

---
+++ DSA-revision/LinkedList/M28L3.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node(int val)
+        {
+            this->val=val;
+            this->next=NULL;
+        }
+};
+int main()
+{
+      Node a(10);
+    Node b(20);
+    Node c(30);
+    Node d(40);
+
+    a.next=&b;
+    b.next=&c;
+    c.next=&d;
+
+    Node temp=a;
+    while(1)// while(temp.next != NULL) this condition wont print 40 so we changed the condition and added a if condition 
+    {
+        cout<<temp.val<<" ";
+        if(temp.next==NULL) break;
+        temp=*(temp.next);
+    }
+    
+}
