450445811b5357542523e7905c9fca4fbd11998a
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-09-18 11:14:32 +0000

    implementation of doubly linked list

---
+++ DSA-revision/LinkedList/M30L1.cpp
@@ -0,0 +1,46 @@
+
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;  
+    }
+};
+void display(Node* head)
+{
+    while(head)
+    {
+        cout<<head->val<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    Node* a=new Node(10);
+    Node* b=new Node(20);
+    Node* c=new Node(30);
+    Node* d=new Node(40);
+    Node* e=new Node(50);
+
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    d->next=e;
+
+    b->prev=a;
+    c->prev=b;
+    d->prev=c;
+    e->prev=d;
+
+    display(a);
+
+}
